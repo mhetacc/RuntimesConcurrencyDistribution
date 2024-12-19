a9595205d7018f48b31fc9e6057467568d9ee7f7
mhetacc <marco.bello.vi@gmail.com>
2024-12-19 17:47:48 +0100

    looping_timer.py

--- POCs/threading/looping_timer.py
+++ POCs/threading/looping_timer.py
@@ -3,5 +3,7 @@ from threading import Timer
 class LoopTimer(Timer):
 
     def run(self):
+        # makes 'if' of Timer.run() a 'while'
+        # returns False each time times out
         while not self.finished.wait(self.interval):
             self.function(*self.args, **self.kwargs)
