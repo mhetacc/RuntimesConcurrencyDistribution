962ec299634e594b787e0c055bf1936c156c32a0
mhetacc <marco.bello.vi@gmail.com>
2024-12-19 18:52:43 +0100

    th_clientPOC.py server + thread loop timer + exposed fun

--- POCs/threading/th_clientPOC.py
+++ POCs/threading/th_clientPOC.py
@@ -1,24 +1,26 @@
 from xmlrpc.server import SimpleXMLRPCServer
 import xmlrpc.client
-#import threading
 import looping_timer
+import datetime
 
-# have a timer which calls back print on the server side
 
 class Client(SimpleXMLRPCServer):
     def __init__(self):
-        super().__init__(('localhost',8080))
+        # creates itself (ie server start-up)
+        SimpleXMLRPCServer.__init__(self, ('localhost',8080))
+
+        # gets a connection to another server via a client proxy
         self.proxy = xmlrpc.client.ServerProxy('http://localhost:8000', allow_none=True)
                 
-        # timer must be last
+        # timer must be last in the constructor
         self.timer = looping_timer.LoopTimer(1.0, self.callback)
         self.timer.start()
         
 
     def callback(self):
-        #self.proxy.server_print('client')
-        print('callback')
-        #self.timer.
+        # while staying active as a server sends 
+        # periodic POST requests to another server
+        self.proxy.server_print('Sono le '+ str(datetime.datetime.now()))
 
 
 ########################################################
