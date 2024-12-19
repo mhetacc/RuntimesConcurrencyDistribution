a1c1d36f3be7e12fb2040cecc3ebfb897a6cad97
mhetacc <marco.bello.vi@gmail.com>
2024-12-19 17:52:32 +0100

    looping_timer.py docstring

--- POCs/threading/looping_timer.py
+++ POCs/threading/looping_timer.py
@@ -1,6 +1,7 @@
 from threading import Timer
 
 class LoopTimer(Timer):
+    """Subclass of threading.Timer: starts a timer that loops in a separate thread, can call a callback function each time it runs out"""
 
     def run(self):
         # makes 'if' of Timer.run() a 'while'
