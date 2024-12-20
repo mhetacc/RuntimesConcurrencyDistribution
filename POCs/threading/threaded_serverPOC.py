f991899f1f0daa680270917821759a495c5ec300
mhetacc <marco.bello.vi@gmail.com>
2024-12-20 13:51:17 +0100

    threaded_serverPOC.py format

--- POCs/threading/threaded_serverPOC.py
+++ POCs/threading/threaded_serverPOC.py
@@ -3,19 +3,7 @@ import threading
 import datetime
 import time
 
-
-
-#with SimpleXMLRPCServer(('localhost', 8000), allow_none=True) as server:
-#
-# 
-#    def server_print(string):
-#        print(string)
-#
-#    server.register_function(server_print)
-#
-#    server.serve_forever()
-
-
+# enclose server in a callable function
 def handle_server():
     with SimpleXMLRPCServer(('localhost', 8080), allow_none=True) as server:
         def just_return(value):
@@ -25,10 +13,11 @@ def handle_server():
         server.serve_forever()
 
 
-
+# pass all server stuff to a separate thread
 threading.Thread(target=handle_server).start()
 
 
+# this is then allowed to run while server does so in a separate thread
 while True:
     time.sleep(1)
     print(str(datetime.datetime.now()))
