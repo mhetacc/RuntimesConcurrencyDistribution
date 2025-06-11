24d6c1da0490715f81867745af53bd4b3aa88fb3
mhetacc <marco.bello.vi@gmail.com>
2025-06-11 13:18:55 +0200

    raft_node: send pygame commands created on ui

--- raftian/raft/bobs/bob1.py
+++ raftian/raft/bobs/bob1.py
@@ -1,5 +1,28 @@
 from xmlrpc.server import SimpleXMLRPCServer
 
+from pathlib import Path
+import logging
+import datetime
+
+############################## logs ###############################
+
+# logs are in the form "logs/filename/datetime.filename.log"
+filename = 'bob1'
+
+# create logger and makes it so that it record any message level
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO, encoding='utf-8')
+
+
+logpath = Path(f'logs/{filename}/{datetime.datetime.now()}.{filename}.log')
+
+# logger handler to sent all to correct path
+filehandle = logging.FileHandler(logpath)
+logger.addHandler(filehandle)
+
+########################################################################
+
+
 # needs to be Raft node but with register_function(append_entries_rpc)
 
 with SimpleXMLRPCServer(('localhost', 8001), allow_none=True) as server:
@@ -7,6 +30,9 @@ with SimpleXMLRPCServer(('localhost', 8001), allow_none=True) as server:
     def append_entries_rpc(entries, term, commit_index):
         print(f"Received append_entries_rpc with term: {term} and commit_index: {commit_index}")
         print(f"Entries: {entries}")
+
+        logger.info(f"Received append_entries_rpc with term: {term} and commit_index: {commit_index}")
+        logger.info(f"Entries: {entries}")  
         # Here you would implement the logic to handle the append entries RPC
         # For now, just return a success message
         return (True, 0)
--- raftian/raft/raft_node.py
+++ raftian/raft/raft_node.py
@@ -7,6 +7,7 @@ import threading
 import concurrent.futures
 import xmlrpc.client
 from queue import Queue
+import pygame
 
 # testing purposes
 # inside function explicit global keyword
@@ -17,7 +18,7 @@ pygame_commands.put('cmd3')
 pygame_commands.put('cmd4')
 
 
-
+# TODO: put class in separate file
 class Raft(SimpleXMLRPCServer):
     class Mode(Enum):
         LEADER = 1
@@ -173,6 +174,8 @@ class Raft(SimpleXMLRPCServer):
         """
         Sends entries to all followers, for each of them traverse backwards through self.log until it finds the last common entry. 
         This is done passively: if follower reject entries, add last one not included from self.log and send again.
+
+        TODO: this needs to be fired every .5s or so, hence either a separate looping timer or count timer clicks some way or another or fire from service actions() 
         """
 
         # TODO pygame commands translation can be decoupled 
@@ -366,7 +369,7 @@ bob2 = Raft.Server(2, 'localhost', 8002, 100)
 bob3 = Raft.Server(3, 'localhost', 8003, 100)
 bob4 = Raft.Server(4, 'localhost', 8004, 100)
 
-bobs_cluster : list[Raft.Server] = [bob1, bob2] # testing purposes
+bobs_cluster : list[Raft.Server] = [bob1] # testing purposes
 
 # enclose server in a callable function
 def handle_server():
@@ -385,7 +388,157 @@ def handle_server():
 
 
 # pass all server stuff to a separate thread
-thread = threading.Thread(target=handle_server)
-thread.start()
-thread.join()
+server_thread = threading.Thread(target=handle_server)
+server_thread.start()
 
+###################################################################################
+################################       PYGAME      ################################
+###################################################################################
+
+def run_pygame():
+    global pygame_commands
+
+    pygame.init()
+
+    GREY = (125, 125, 125)
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+
+    # create game window and set res 1000x1200
+    DISPLAY = pygame.display.set_mode((1000, 1200))
+
+    clock = pygame.time.Clock()
+
+    # creates default font 
+    font = pygame.font.Font(None, 60)
+
+    # renders header and footer texts
+    toptext = font.render("Top Text", False, BLACK)
+    bottomtext = font.render("Bottom Text", False, BLACK)
+
+
+    ############################### base surfaces #####################################
+
+    # creates rects to constrain surfaces 
+    # Rect((x0, y0), (width, height))
+    rect_main = pygame.Rect(0, 100, 1000, 1000)
+    rect_header = pygame.Rect(0, 0, 1000, 100)
+    rect_footer = pygame.Rect(0, 1100, 1000, 100)
+
+    # creates surfaces where UI elements are drawn upon
+    # Surface((width, height))
+    mainwindow = pygame.Surface((1000, 1000))
+    header = pygame.Surface((1000, 100))
+    footer = pygame.Surface((1000, 100))
+
+    # color surfaces
+    mainwindow.fill(GREY)
+    header.fill(WHITE)
+    footer.fill(WHITE)
+
+
+    # vertical "game map" lines (on main window)
+    pygame.draw.line(mainwindow, BLACK, (250, 0), (250, 1000), width=1)
+    pygame.draw.line(mainwindow, BLACK, (500, 0), (500, 1000), width=1)
+    pygame.draw.line(mainwindow, BLACK, (750, 0), (750, 1000), width=1)
+
+    # horizontal "game map" lines (on main window)
+    pygame.draw.line(mainwindow, BLACK, (0, 250), (1000, 250), width=1)
+    pygame.draw.line(mainwindow, BLACK, (0, 500), (1000, 500), width=1)
+    pygame.draw.line(mainwindow, BLACK, (0, 750), (1000, 750), width=1)
+
+
+    # draw surfaces on DISPLAY surface "binded" on and by their respective rects
+    # maybe rects were not necessary but they provide nice features nonetheless 
+    DISPLAY.blit(mainwindow, rect_main) 
+    DISPLAY.blit(header, rect_header)
+    DISPLAY.blit(footer, rect_footer)
+
+    ###################################################################################
+
+    ########################### header and footer texts ###############################
+
+    # since all boxes' positions starts from top left corner, to center them we
+    # must calculate the offsets which are width/2 and height/2
+
+    # header
+    toptext_rect = toptext.get_rect()
+    xoffset = toptext_rect.width/2
+    yoffset = toptext_rect.height/2
+
+    DISPLAY.blit(toptext, (rect_header.centerx - xoffset, rect_header.centery - yoffset))
+
+    # footer
+    bottomtext_rect = bottomtext.get_rect()
+    xoffset = bottomtext_rect.width/2
+    yoffset = bottomtext_rect.height/2
+
+    DISPLAY.blit(bottomtext, (rect_footer.centerx - xoffset, rect_footer.centery - yoffset))
+
+    #################################################################################
+
+
+    # create "button" rect and its surface
+    # and blit it to display
+    rect_btn = pygame.Rect(585, 685, 80, 80)
+    button = pygame.Surface((80,80))
+    button.fill(RED)
+    DISPLAY.blit(button, rect_btn)
+
+
+    button_counter = 1
+
+    # MAIN LOOP
+    while True:
+        # Process player inputs.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                raise SystemExit
+
+            # if mouse left button is clicked 
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+
+                # gets mouse position
+                pos = pygame.mouse.get_pos()
+
+                # test if a point (ie mouse) is inside the rectangle (ie button)
+                if rect_btn.collidepoint(pos):
+
+                    # tells server that button has been pressed
+                    pygame_commands.put(f'Button counter = {button_counter}')
+                    button_counter += 1
+
+                    # change header text and renders it
+                    toptext = font.render("Button Pressed", False, BLACK)
+
+                    # calculate offset
+                    toptext_rect = toptext.get_rect()
+                    xoffset = toptext_rect.width/2
+                    yoffset = toptext_rect.height/2
+
+                    # must first re-draw header surface otherwise previous text remains 
+                    # then draws changed header text
+                    DISPLAY.blit(header, rect_header)
+                    DISPLAY.blit(toptext, (rect_header.centerx - xoffset, rect_header.centery - yoffset))
+
+
+        # Do logical updates here.
+        # ...
+
+
+        # Render the graphics here.
+        # ...
+
+
+        # we want to limit display refresh speed
+        pygame.display.flip()  # Refresh on-screen display
+        clock.tick(60)         # sets framerate
+
+
+pygame_thread = threading.Thread(target=run_pygame)
+pygame_thread.start()
+
+server_thread.join()
+pygame_thread.join()
